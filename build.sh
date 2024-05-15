c57a16c4ef4001df04409e130e4f6c6f6e00ef1b
Asir Shahid <asirshahid@protonmail.com>
2024-05-15 00:27:21 -0400

    Install packages

--- build.sh
+++ build.sh
@@ -14,9 +14,12 @@ RELEASE="$(rpm -E %fedora)"
 
 # this installs a package from fedora repos
 rpm-ostree install screen
+rpm-ostree install tmux
+rpm-ostree install ncdu
+rpm-ostree install neovim
 
 # this would install a package from rpmfusion
-# rpm-ostree install vlc
+rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
